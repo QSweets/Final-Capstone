0093e9078f1872a0fc1f213dbb40c89d6dc4ce7f
Daiyon Winters <Winters10k@gmail.com>
2023-12-06 13:35:41 -0500

    pushing backend server fixes

--- java/src/main/java/com/techelevator/controller/MonsterController.java
+++ java/src/main/java/com/techelevator/controller/MonsterController.java
@@ -2,6 +2,7 @@ package com.techelevator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -9,22 +10,24 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @CrossOrigin
+@PreAuthorize("isAuthenticated()")
 public class MonsterController {
 
-    private final String MONSTER_API_BASE_URL = "https://www.dnd5eapi.co/api/monsters";
-    private final RestTemplate restTemplate;
+    // private final String MONSTER_API_BASE_URL = "https://www.dnd5eapi.co/api/monsters";
+    // private final RestTemplate restTemplate;
 
     @Autowired //used for automatic dependency injection
-    public MonsterController(RestTemplate restTemplate) { /** This is the dependency that Spring will inject **/
-        this.restTemplate = restTemplate;
+    public MonsterController() { /** This is the dependency that Spring will inject **/
+   //     this.restTemplate = restTemplate;
     }
 
-    public ResponseEntity<String> getRandomMonster() {
-        String dndApi = MONSTER_API_BASE_URL + ""; /** to get information about a random monster **/
-        String response = restTemplate.getForObject(dndApi, String.class); /** is making a GET request to the specified URL (dndApi) it is stored as a String **/
+    /*public ResponseEntity<String> getRandomMonster() {
+        String dndApi = MONSTER_API_BASE_URL + ""; *//** to get information about a random monster **//*
+        String response = restTemplate.getForObject(dndApi, String.class); *//** is making a GET request to the specified URL (dndApi) it is stored as a String **//*
 
-        return ResponseEntity.ok(response); /** Used to Construct and return a HTTP response with a status of 200 along with the response body **/
-    }
+        return ResponseEntity.ok(response); *//** Used to Construct and return a HTTP response with a status of 200 along with the response body **//*
+
+    }*/
 
 
 }
