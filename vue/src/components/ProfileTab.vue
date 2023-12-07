1ad545538f00834afde30e289043530fc938db53
Daiyon Winters <Winters10k@gmail.com>
2023-12-07 17:20:07 -0500

    Pushing front end updates and getting backend work

---
+++ vue/src/components/ProfileTab.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="dropdown">
+    <select class="dropbtn">Dropdown
+      <button>milk</button>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </select>
+  </div>
+</template>
