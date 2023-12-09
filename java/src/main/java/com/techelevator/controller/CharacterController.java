c0b3f14c9283fc502df0b94b25279c7a18562a20
lexusmfannin <lexusmfannin@gmail.com>
2023-12-08 22:35:53 -0500

    daos and jdbc

--- java/src/main/java/com/techelevator/controller/CharacterController.java
+++ java/src/main/java/com/techelevator/controller/CharacterController.java
@@ -1,12 +1,30 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.CharacterDao;
+import com.techelevator.model.Character;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping()
-public class CharacterController {
+import java.util.List;
 
+@RestController
+@RequestMapping("/users/characters")
+@PreAuthorize("isAuthenticated()")
+public class CharacterController {
+    private CharacterDao characterDao;
+
+    public CharacterController(CharacterDao characterDao) {
+        this.characterDao = characterDao;
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public List<Character> getCharactersByUsername() {
+        characterDao.getCharactersByUsername();
+        List<Character> characters = characterDao.getCharactersByUsername();
+
+    }
 
 
 
--- java/src/main/java/com/techelevator/dao/JdbcCharacterDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcCharacterDao.java
@@ -1,46 +1,54 @@
 package com.techelevator.dao;
 
+import com.techelevator.exception.DaoException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class JdbcCharacterDao implements CharacterDao{
-    //Using TEnmo as a reference
-
+public class JdbcCharacterDao /*implements CharacterDao*/ {
     private JdbcTemplate jdbcTemplate;
+
     public JdbcCharacterDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
-
-    @Override
-    public List<Character> getCharactersByUsername() {
-//        String sql = "SELECT * FROM character";
-//
-//        List<Character> characters = jdbcTemplate.query(sql, (resultSet, rowNum) -> {
-//            Character character = new Character();
-//            character.
-//        })
-
-        return null;
-    }
-
-    @Override
-    public Character getCharacterById(int characterId) {
-        return null;
-    }
-
-    @Override
-    public Character createCharacter(Character character) {
-        return null;
-    }
-
-    @Override
-    public Character updateCharacter(Character character) {
-        return null;
-    }
-
-    @Override
-    public int deleteCharacterById(int id) {
-        return 0;
-    }
 }
+
+//    @Override
+//    public List<Character> getCharactersByUsername(String username) {
+//        //Change this select *
+//        List<Character> characters = new ArrayList<>();
+//        String sql = "SELECT * FROM character WHERE username = ? ";
+//        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, username);
+//        try {
+//            while (results.next()) {
+//                Character character = mapRowToCharacter(results);
+//                characters.add(character);
+//            }
+//        } catch (CannotGetJdbcConnectionException e) {
+//            throw new DaoException("Unable to connect to server or database", e);
+//
+//        return characters;
+//    }
+//
+//    @Override
+//    public Character createCharacter(Character character) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Character updateCharacter(Character character) {
+//        return null;
+//    }
+//
+//    @Override
+//    public int deleteCharacterById(int id) {
+//        return 0;
+//    }
+//
+//
+//    private Character mapRowToCharacter(SqlRowSet rs) {
+//
+//}
