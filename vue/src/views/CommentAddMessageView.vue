6e6539034cb3830412563eddcd9061d011f49606
Kaley Hamilton <kaleyhamilton6@gmail.com>
2023-12-09 18:29:12 -0500

    Comment Operations

---
+++ vue/src/views/CommentAddMessageView.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Add New Message</h1>
+    <comment-message-form v-bind:message="message" />
+  </template>
+  
+  <script>
+  import CommentMessageForm from '../components/CommentMessageForm.vue';
+  
+  export default {
+    components: {
+      CommentMessageForm
+    },
+    data() {
+      return {
+        message: {
+          id: 0,
+          topicId: Number(this.$route.params.topicId),
+          created: new Date(),
+          title: '',
+          messageText: ''
+        },
+      }
+    } 
+  };
+  </script>
+  
+  <style scoped>
+  </style>
