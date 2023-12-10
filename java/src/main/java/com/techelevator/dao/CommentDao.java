a17c88d9c49db65114f7e6cf02749b6ec97d6711
Kaley Hamilton <kaleyhamilton6@gmail.com>
2023-12-10 16:14:01 -0500

    Comment Backend

--- java/src/main/java/com/techelevator/dao/CommentDao.java
+++ java/src/main/java/com/techelevator/dao/CommentDao.java
@@ -1,6 +1,13 @@
 package com.techelevator.dao;
+import com.techelevator.model.Comment;
+import java.util.List;
 
 public interface CommentDao {
+    List<Comment> getAllComments();
+    Comment createComment(Comment comment);
+    Comment updateComment(Comment comment);
+    Comment deleteComment(int userId, int commentId);
+    List<Comment> getCommentsByUserId(int userId);
 
 
 
