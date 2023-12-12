0f6933a716be5b928eb5653e15d766055962ca65
lexusmfannin <lexusmfannin@gmail.com>
2023-12-11 21:55:46 -0500

    updates

--- vue/src/views/SocialView.vue
+++ vue/src/views/SocialView.vue
@@ -1,19 +1,10 @@
 <template>
-<<<<<<< HEAD
   <div class="social">
     <h1>Let's Talk About It</h1>
     <!-- This is to navigate to the homepage -->
     <router-link to="/" v-if="$store.state.token !== ''">Home</router-link> &nbsp; | &nbsp;
     <!-- navigate to the "logout" route -->
     <router-link to="/logout" v-if="$store.state.token !== ''">Logout</router-link>
-=======
-  <div >
-    <header class="flex">
-      <h1>Topics</h1>
-      <button class="btn-add" v-on:click="$router.push({ name: 'CommentAddTopicView' })">Add Topic</button>
-    </header>
-    <comment-topic-list v-bind:topics="topics"/>
->>>>>>> 79689c3edfb9caa3d3f8d5555eec03cf127e2b7e
   </div>
   <global-logo-header />
   <social v-bind:message="message" />
